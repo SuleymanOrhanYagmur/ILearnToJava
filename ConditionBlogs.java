a7e3193ef72e19e286610ff595e8a1827d949fc2
Suleyman Orhan Yağmur <suleymanorhan111@gmail.com>
2023-09-20 18:28:40 +0300

    Learn Condition Blogs

---
+++ ConditionBlogs.java
@@ -0,0 +1,22 @@
+
+public class ConditionBlogs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 20;
+		if (num1 <30) {
+			System.out.println("Numaram 30 dan küçüktür");
+
+			
+		}else if(num1 == 30) {
+			System.out.println("Sayı 30 a eşit ise");
+		}else {
+			System.out.print("sayi 30 dan küçüktür");
+		}
+		
+		
+		 
+		
+	}
+
+}
