fc7310538d4627e34a4d60be8fb5576cba62eb62
Suleyman Orhan Yağmur <suleymanorhan111@gmail.com>
2023-09-20 18:13:53 +0300

    Javada değişken tanımlamayı onu ve kendi yazımı ekrana yazdırmayı öğrendim

---
+++ main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int numberofstudents = 10;
+		String message ="Number of students: ";
+		
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello java (My first java code) ");
+		System.out.println("Ogrenci sayim: "+numberofstudents);
+		System.out.println(message + numberofstudents);
+		
+		
+
+	}
+
+}
