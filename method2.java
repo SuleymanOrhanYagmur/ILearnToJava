1b65e1992286d6ea8b670d5a6ba4c560e87320ba
Suleyman Orhan Yağmur <suleymanorhan111@gmail.com>
2023-09-27 14:12:33 +0300

    methodlar

---
+++ method2.java
@@ -0,0 +1,55 @@
+
+public class method2 {
+
+	public static void main(String[] args) {
+		String mesaj = "Bugün hava çok güzel.";
+		String yeniMesaj = mesaj.substring(0,2);// 0. dan 2. ye g ye kadar değerleri alıcaktı
+		System.out.println(yeniMesaj);
+		
+		int sayi = topla(5,7);
+		System.out.println(sayi);
+		
+		// int toplam =  topla2(2,3,4,5,6);
+	}
+	
+	
+	public static void ekle() {
+		System.out.println("Eklendi");
+		
+		
+	}
+	
+	
+	public static void sil() {
+		System.out.println("Silindi");
+		
+		
+		
+	}
+	
+	public static void guncelle() {
+		System.out.println("Güncellendi ");
+		
+		
+	}
+	
+	public static int topla(int a, int b) {
+		
+		return a+ b;
+		
+		
+	}
+	
+	// YUKARDAKİ 2 TANE DEĞERİİÇİN SAĞLIYOR İSTEDİĞİMİZ KADAR HALİNİ AŞAĞIDAKİ GİBİ SAĞLARIZ
+	/* TEKRAR İNCELE
+	public static int topla2(int... sayilar){
+		int toplam = 0;
+		for(int sayi:sayilar) {
+			toplam+=sayi;
+			
+		}
+		
+		
+	}
+  */
+}
